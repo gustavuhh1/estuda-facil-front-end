1b709056064155db10cdd8372557f5fa2825536c
ALUNO T07 <alunot07@unifor.local>
2025-05-08 19:22:26 -0300

    feat: adicionando CRUD Aluno

--- src/app/painel-admin/page.tsx
+++ src/app/painel-admin/page.tsx
@@ -6,7 +6,6 @@ import ContainerApiTest from "./components/ContainerApiTest";
 export default function PainelAdminPage() {
   return (
     <div>
-      {/* Adicionar tag de Auth */}
       <Nav />
       <h1 className="text-3xl py-6 px-12">Painel Admin (Restrito)</h1>
       <ContainerApiTest />
